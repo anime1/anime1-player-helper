98782c90796ee81b64e11d0d6fbda608185c523c
Anime1 <31569396+anime1@users.noreply.github.com>
2018-11-06 13:42:06 +0000

    v1.1

---
+++ background.js
@@ -0,0 +1,42 @@
+'use strict';
+console.log(chrome.runtime.getManifest().version);
+
+// Check extension version
+chrome.runtime.onMessageExternal.addListener(
+	(message, sender, sendResponse) => {
+        console.log(message);
+	if (message == 'version') {
+		const manifest = chrome.runtime.getManifest();
+		sendResponse({
+			type: 'success',
+			version: manifest.version
+		});
+		return true;
+	}
+	return true;
+});
+
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	(details) => {
+	console.log(details);
+	for (var i = 0; i < details.requestHeaders.length; ++i) {
+		if (details.requestHeaders[i].name === 'Referer') {
+			details.requestHeaders[i].value = "https://www.facebook.com/"
+				break;
+		} else {
+			details.requestHeaders.push({
+				name: 'Referer',
+				value: 'https://www.facebook.com/'
+			});
+			break;
+		};
+	}
+	return {
+		requestHeaders: details.requestHeaders
+	};
+}, {
+	urls: ["https://video.xx.fbcdn.net/*"],
+	types: ["media"]
+},
+	["blocking", "requestHeaders"]);
