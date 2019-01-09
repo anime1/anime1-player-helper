f38b43c0540733ad78b0ae953224fb3e2c8356dd
Anime1 <31569396+anime1@users.noreply.github.com>
2019-01-09 03:09:17 +0000

    v1.3

--- background.js
+++ background.js
@@ -2,37 +2,41 @@
 
 // Check extension version
 chrome.runtime.onMessageExternal.addListener(
-	(message, sender, sendResponse) => {
-	if (message == 'version') {
-		const manifest = chrome.runtime.getManifest();
-		sendResponse({
-			type: 'success',
-			version: manifest.version
-		});
-		return true;
-	}
-	return true;
-});
+    (message, sender, sendResponse) => {
+        if (message == 'version') {
+            const manifest = chrome.runtime.getManifest();
+            sendResponse({
+                type: 'success',
+                version: manifest.version
+            });
+            return true;
+        }
+        return true;
+    });
+
+
+var extraInfoSpec = ['blocking', 'requestHeaders'];
+if (chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty('EXTRA_HEADERS')) extraInfoSpec.push('extraHeaders');
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-	(details) => {
-	for (var i = 0; i < details.requestHeaders.length; ++i) {
-		if (details.requestHeaders[i].name === 'Referer') {
-			details.requestHeaders[i].value = "https://www.facebook.com/"
-				break;
-		} else {
-			details.requestHeaders.push({
-				name: 'Referer',
-				value: 'https://www.facebook.com/'
-			});
-			break;
-		};
-	}
-	return {
-		requestHeaders: details.requestHeaders
-	};
-}, {
-	urls: ["https://video.xx.fbcdn.net/*"],
-	types: ["media"]
-},
-	["blocking", "requestHeaders"]);
+    (details) => {
+        for (var i = 0; i < details.requestHeaders.length; ++i) {
+            if (details.requestHeaders[i].name === 'Referer') {
+                details.requestHeaders[i].value = "https://www.facebook.com/"
+                break;
+            } else {
+                details.requestHeaders.push({
+                    name: 'Referer',
+                    value: 'https://www.facebook.com/'
+                });
+                break;
+            };
+        }
+        return {
+            requestHeaders: details.requestHeaders
+        };
+    }, {
+        urls: ["https://video.xx.fbcdn.net/*"],
+        types: ["media", "xmlhttprequest"]
+    },
+    extraInfoSpec);
